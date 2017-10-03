43534027e875b68898d78644b2601f31f1fab391
Sebastien Fichot <fichot.sebastien@gmail.com>
2017-10-03 18:15:36 +0200

    Removed offline

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,12 +4,5 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-styled-components`,
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: `UA-93349937-2`,
-      },
-    },
-    `gatsby-plugin-offline`,
   ],
 }
